37bcd9a4aa37692ad4e55eb8a70add5bb52696a3
sonali_sohoni <ssohoni@camsbycbs.com>
2021-12-19 11:15:17 -0500

    validation is now complete

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,49 +1,56 @@
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 const PizzaSchema = new Schema(
-  {
-    pizzaName: {
-      type: String
-    },
-    createdBy: {
-      type: String
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: createdAtVal => dateFormat(createdAtVal)
-    },
-    size: {
-      type: String,
-      default: 'Large'
-    },
-    toppings: [],
-    comments: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Comment'
-      }
-    ]
-  },
-  {
-    toJSON: {
-      virtuals: true,
-      getters: true
-    },
-    // prevents virtuals from creating duplicate of _id as `id`
-    id: false
-  }
+	{
+		pizzaName: {
+			type: String,
+			trim: true,
+			required: "Pizza Name is a required field",
+		},
+		createdBy: {
+			type: String,
+			trim: true,
+			required: "CreatedBy is a required field",
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: (createdAtVal) => dateFormat(createdAtVal),
+		},
+		size: {
+			type: String,
+			default: "Large",
+			required: true,
+			enum: ["Personal", "Small", "Medium", "Large", "Extra Large"],
+			default: "Large",
+		},
+		toppings: [],
+		comments: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Comment",
+			},
+		],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true,
+		},
+		// prevents virtuals from creating duplicate of _id as `id`
+		id: false,
+	}
 );
 
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.reduce(
-    (total, comment) => total + comment.replies.length + 1,
-    0
-  );
+PizzaSchema.virtual("commentCount").get(function () {
+	return this.comments.reduce(
+		(total, comment) => total + comment.replies.length + 1,
+		0
+	);
 });
 
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 module.exports = Pizza;
